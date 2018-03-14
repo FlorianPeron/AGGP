adb56cce24a1318c37a00a91d5226a29f4cfaae1
Lea Douchet <lea.douchet@insa-lyon.fr>
2018-03-14 14:16:35 +0100

    Degree des noeuds

--- Network.py
+++ Network.py
@@ -14,7 +14,7 @@ class sexualNetwork(Graph):
 	def __init__(self,n,m):
 		self.__dict__ = nx.barabasi_albert_graph(n,m).__dict__.copy()
 		self.fitness = 0
-		self.nbr_noeud
+		self.nbr_noeud = n
 	
 	def mutation(self,proba):
 		for i in range(len(self.nodes)):
@@ -29,29 +29,38 @@ class sexualNetwork(Graph):
 	
 	def Fitness(self) : 
 		## Invariant d'echelle
-		deg = self.network.degree()
-		print(deg)
+		deg = Degree_distribution()
+		
+		
 		## Diametre 
-		D = nx.diameter(self.network)
+		D = nx.diameter(self)
 		D_difference = 1-abs(1-D/log(log(self.nbr_noeud)))
 		
 		## Coefficient de clustering
 		cc = node_clustering()
 		cc_difference = 1-abs(1-cc/gamma)
 		
-
+	def Degree_distribution(self) :
+		#Obtaining list of degree values
+		data_deg=dict(self.degree()).values()
+		data_deg_val=list(data_deg)
+		fit=pl.Fit(data_deg_val, discrete=True) 
+		return(fit.power_law.alpha)
 
 	def node_clustering(self):
-		coefficients_clustering_nodes = nx.clustering(self.network, nodes=None, weight=None)
+		coefficients_clustering_nodes = nx.clustering(self, nodes=None, weight=None)
 		coefficients = list(coefficients_clustering_nodes.values())
 		fit = pl.Fit(coefficients, discrete = True)
 		return(fit.power_law.alpha)
+		
+		
+
 G = sexualNetwork(10,2)
 
 nx.draw_circular(G, with_labels=True, font_weight='bold')
 
 plt.subplot(212)
 G.mutation(0.05)
-nx.draw_circular(G, with_labels=True, font_weight='bold')
+print(G.Degree_distribution())
 
 plt.show()
