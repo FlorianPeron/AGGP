8c3748cf86e8dca351f267f68dc78af18e900b4b
Florian Peron <florian.peron@insa-lyon.fr>
2018-03-14 14:00:56 +0100

    add CrossOver

--- Network.py
+++ Network.py
@@ -12,7 +12,7 @@ class sexualNetwork(Graph):
 		self.__dict__ = nx.barabasi_albert_graph(n,m).__dict__.copy()
 		self.fitness = 0
 	
-	def mutation(self,proba):
+	def Mutation(self,proba):
 		for i in range(len(self.nodes)):
 			P = rn.uniform(0,1)
 			if (P<proba):
@@ -23,20 +23,43 @@ class sexualNetwork(Graph):
 					partner = rn.choice(list(self.nodes))
 					self.add_edge(i,partner)
 	
+	def CrossOver(self,proba,n,graph):
+		P = rn.uniform(0,1)
+		if (P<proba):
+			nodes_to_cross = rn.sample(list(self.nodes()),n)
+			print(nodes_to_cross)
+			for n in nodes_to_cross:
+				e = list(self.edges(n))
+				self.remove_edges_from(e)
+			for n in nodes_to_cross:
+				e = list(graph.edges(n))
+				self.add_edges_from(e)
+	
 	def Fitness(self) : 
 		## Invariant d'echelle
 		## Petit monde
 		## Coefficient de clustering
 		self.fitness = 0
-		
+	
+	
 
 plt.subplot(211)
-G = sexualNetwork(20,2)
+G1 = sexualNetwork(20,1)
+nx.draw_circular(G1, with_labels=True, font_weight='bold')
 
-nx.draw_circular(G, with_labels=True, font_weight='bold')
 
 plt.subplot(212)
-G.mutation(0.05)
-nx.draw_circular(G, with_labels=True, font_weight='bold')
+G1.Mutation(0.1)
+nx.draw_circular(G1, with_labels=True, font_weight='bold')
+
+'''
+plt.subplot(312)
+G2 = sexualNetwork(20,1)
+nx.draw_circular(G2, with_labels=True, font_weight='bold')
+
+plt.subplot(313)
+G2.CrossOver(1,3,G1)
+nx.draw_circular(G2, with_labels=True, font_weight='bold')
+'''
 
 plt.show()
