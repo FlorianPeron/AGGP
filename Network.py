3bc774c747ef83c4db835976f5211f0dd434bd5f
Florian Peron <florian.peron@insa-lyon.fr>
2018-03-14 10:44:31 +0100

    add init graph

--- Network.py
+++ Network.py
@@ -1,9 +1,19 @@
 import networkx as nx
 from networkx import Graph
+from matplotlib import use
+use('qt4agg')
+import matplotlib.pyplot as plt
 
 
-class sexualNetwork(Graph):
-    def __init__(self,n,m):
-        self = nx.barabasi_albert_graph(n,m)
+class sexualNetwork():
+	def __init__(self,n,m):
+		self.network = nx.barabasi_albert_graph(n,m)
+		self.fitness = 0
 
 
+
+G = sexualNetwork(10,2)
+
+nx.draw(G.network, with_labels=True, font_weight='bold')
+
+plt.show()
